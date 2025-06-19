296e1d0ab5089d83322bd7248f7f987aa743571b
lvppaul <levinhphat790@gmail.com>
2025-06-19 14:49:01 +0700

    FE - Authentication + Route + Logic Login

--- FE/FPT-EduTrack/src/main.tsx
+++ FE/FPT-EduTrack/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.tsx";
+import { AuthProvider } from "./authentication/AuthProvider.tsx";
 import AppRoutes from "./routes/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- FE/FPT-EduTrack/src/pages/Login.tsx
+++ FE/FPT-EduTrack/src/pages/Login.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@mui/material";
-
+import { userLogin } from "../service/userService";
+import { useAuth } from "../context/AuthContext";
+import { validateLogin } from "../utils/validation";
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const { handleLogin } = useAuth();
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // Ngăn chặn reload trang
+    setError(null);
+    setLoading(true);
+    const trimmedEmail = email.trim();
+    const validationErrors = validateLogin(trimmedEmail, password);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      setLoading(false);
+      return;
+    }
+
+    setErrors({});
+
+    try {
+      const { accessToken, refreshToken } = await userLogin(
+        trimmedEmail,
+        password
+      );
+
+      handleLogin(accessToken, refreshToken);
+    } catch (error: unknown) {
+      setError(
+        error instanceof Error ? error.message : "Login failed at Login.tsx"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div
       className="h-screen w-full bg-cover flex items-center justify-center "
@@ -20,13 +62,24 @@ export default function SignInPage() {
           >
             Sign In
           </Typography>
-          <form className="flex flex-col gap-3">
-            <TextField label="Email" variant="outlined" fullWidth />
+          <form onSubmit={onSubmit} className="flex flex-col gap-3">
+            <TextField
+              label="Email"
+              variant="outlined"
+              fullWidth
+              onChange={(e) => setEmail(e.target.value)}
+              error={!!errors.email}
+              helperText={errors.email}
+              autoFocus
+            />
             <TextField
               label="Password"
               type="password"
               variant="outlined"
+              onChange={(e) => setPassword(e.target.value)}
               fullWidth
+              error={!!errors.password}
+              helperText={errors.password}
             />
             <div className="flex items-center justify-end mt-4">
               <input type="checkbox" id="rememberMe" className="mr-2 h-4 w-4" />
@@ -38,9 +91,15 @@ export default function SignInPage() {
               type="submit"
               variant="contained"
               className="!bg-black hover:!bg-gray-800 text-white rounded-full py-2"
+              disabled={loading}
             >
               Sign in
             </Button>
+            {error && (
+              <Typography color="error" sx={{ mt: 1 }}>
+                {error}
+              </Typography>
+            )}
             <div className="text-center mt-4">
               <Typography variant="body2" className="text-gray-600">
                 Forgot password?{" "}
