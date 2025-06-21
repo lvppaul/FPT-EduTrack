fae9e4353e3989a6a559f8035bac89eacb1274ca
MinQan <minhquan141104@gmail.com>
2025-06-21 23:28:28 +0700

    refactor(user&meeting): add MeetingDetails to Entity

---
+++ BE/FPT_EduTrack/FPT_EduTrack.DataAccessLayer/Repositories/MeetingRepository.cs
@@ -0,0 +1,47 @@
+﻿using FPT_EduTrack.DataAccessLayer.Context;
+using FPT_EduTrack.DataAccessLayer.Entities;
+using FPT_EduTrack.DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPT_EduTrack.DataAccessLayer.Repositories
+{
+    public class MeetingRepository : GenericRepository<Meeting>, IMeetingRepository
+    {
+        public MeetingRepository(FptEduTrackContext context) : base(context)
+        {
+        }
+        
+        public async Task<List<Meeting>> GetAllMeetingsAsync()
+        {
+            return await _context.Meetings
+                .Where(m => m.IsDeleted != true)
+                .ToListAsync();
+        }
+
+        public Task<Meeting?> GetMeetingByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Meeting>> GetMeetingsByUserIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Meeting>> GetMeetingsByStatusAsync(int statusId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Meeting>> GetMeetingsByDateAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
