d4653623dcd4f5b14d4dfc4f3dc58b73f44b7ac9
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2025-06-28 21:00:35 +0700

    AI(ai): Grading AI

--- BE/FPT_EduTrack/Api/Program.cs
+++ BE/FPT_EduTrack/Api/Program.cs
@@ -15,6 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpClient();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -64,6 +65,9 @@ builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IMeetingService, MeetingService>();
+builder.Services.AddScoped<IParseResponse, ParseResponse>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMeetingService, MeetingService>();
 
 builder.Services.AddCors(options =>
 {
@@ -100,7 +104,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseRouting();
